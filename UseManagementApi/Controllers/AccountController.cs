8f904af6c5396900e24a1cc41db43e5c5d00a1d6
PlaciDev <brunoplacidev@gmail.com>
2025-06-25 22:26:41 -0300

    cria e configura o AccountController e RegisterViewModel

---
+++ UseManagementApi/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UseManagementApi.Data;
+
+namespace UseManagementApi.Controllers;
+
+[ApiController]
+public class AccountController : ControllerBase
+{
+    [HttpPost("api/accounts/register")]
+    public async Task<IActionResult> Register(
+        [FromServices] ApiDbContext context,
+        [FromBody] )
+    {
+        
+    }
+}
---
+++ UseManagementApi/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UseManagementApi.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "O nome é obrigatório.")]
+    public string Name { get; set; }
+    
+    [Required(ErrorMessage = "O email é obrigatório.")]
+    [EmailAddress(ErrorMessage = "O email é inválido.")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "A senha é obrigatória.")]
+    [MinLength(10, ErrorMessage = "A senha deve conter no minimo 10 caracteres.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&\-_])[A-Za-z\d@$!%*?&\-_]{10,}$",
+        ErrorMessage = "A senha deve conter pelo menos uma letra maiuscula, uma letra minúscula, um número e um caractere especia.")]
+    public string Password { get; set; }
+}
