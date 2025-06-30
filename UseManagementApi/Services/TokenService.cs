ba6b43450834fd9b9c6c71592c33a0ddd12acc9b
PlaciDev <brunoplacidev@gmail.com>
2025-06-29 21:08:51 -0300

    configura o ClaimsIdentity

--- UseManagementApi/Services/TokenService.cs
+++ UseManagementApi/Services/TokenService.cs
@@ -1,4 +1,5 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using UseManagementApi.Models;
@@ -11,8 +12,16 @@ public class TokenService
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var claims = user
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.Name)
+                
+            }),
+            
             Expires = DateTime.UtcNow.AddHours(8),
             
             SigningCredentials = new SigningCredentials(
