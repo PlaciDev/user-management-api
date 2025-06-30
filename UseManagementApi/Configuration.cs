3ed8fef4ad63023071d11971fd0c07cf98486ed3
PlaciDev <brunoplacidev@gmail.com>
2025-06-30 16:51:43 -0300

    cria a minha classe SmtpConfiguration, para implementação do serviço de email
    # Conflicts:
    #	UseManagementApi/Configuration.cs

---
+++ UseManagementApi/Configuration.cs
@@ -0,0 +1,14 @@
+﻿namespace UseManagementApi;
+
+public class Configuration
+{
+    public static SmtpConfiguration Smtp = new();
+    
+    public class SmtpConfiguration  
+    {
+        public string Host { get; set; }
+        public int Port { get; set; } = 25;
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
