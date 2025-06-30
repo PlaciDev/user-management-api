0f1db851fdc5e14cfc19f9ace63e728f2ad0cfa0
PlaciDev <brunoplacidev@gmail.com>
2025-06-30 17:34:35 -0300

    cria e configura o serviço de email

---
+++ UseManagementApi/Services/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace UseManagementApi.Services;
+
+public class EmailService
+{
+    public bool Send(
+        string toName,
+        string toEmail,
+        string subject,
+        string body,
+        string fromName = "DEV - Bruno Placides",
+        string fromEmail = "brunoplacidev@gmail.com")
+    {
+        var smtpClient = new SmtpClient(Configuration.Smtp.Host, Configuration.Smtp.Port);
+        
+        // Configura as credenciais de rede
+        smtpClient.Credentials = new NetworkCredential(Configuration.Smtp.Username, Configuration.Smtp.Password);
+        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        smtpClient.EnableSsl = true;
+        
+        // Cria a mensagem de email
+        var mail = new MailMessage();
+
+        mail.From = new MailAddress(fromEmail, fromName);
+        mail.To.Add(new MailAddress(toEmail, toName));
+        mail.Subject = subject;
+        mail.Body = body;
+        mail.IsBodyHtml = true;
+
+        try
+        {
+            smtpClient.Send(mail);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
