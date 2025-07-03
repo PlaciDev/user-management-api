27b6cc0aff38bb588a011d434ab7b089cd277c20
PlaciDev <brunoplacidev@gmail.com>
2025-07-02 22:34:14 -0300

    configura o mapeamento de User

---
+++ UseManagementApi/Data/Mappins/UserMap.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UseManagementApi.Models;
+
+namespace UseManagementApi.Data.Mappins;
+
+public class UserMap : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("User");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+        
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(50);
+        
+        builder.Property(x => x.Email)
+            .IsRequired()
+            .HasColumnName("Email")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(70);
+        
+        builder.Property(x => x.PasswordHash)
+            .IsRequired()
+            .HasColumnName("PasswordHash")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(255);
+        
+        builder.Property(x => x.IsActive)
+            .IsRequired()
+            .HasColumnName("IsActive")
+            .HasColumnType("BIT");
+        
+        builder.Property(x => x.CreatedAt)
+            .IsRequired()
+            .HasColumnName("CreatedAt")
+            .HasColumnType("DATETIME");
+    }
+}
