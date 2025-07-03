85aa54ec81d578cf6635b03e4d91e36ffd5734f8
PlaciDev <brunoplacidev@gmail.com>
2025-07-02 22:50:44 -0300

    configurar o ContextFactory

---
+++ UseManagementApi/Factory/ApiDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using UseManagementApi.Data;
+
+namespace UseManagementApi.Factory;
+
+public class ApiDbContextFactory : IDesignTimeDbContextFactory<ApiDbContext>
+{
+    public ApiDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json")
+            .Build();
+        
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        
+        var optionsBuilder = new DbContextOptionsBuilder<ApiDbContext>();
+        optionsBuilder.UseSqlServer(connectionString);
+        
+        return new ApiDbContext(optionsBuilder.Options);
+    }
+}
