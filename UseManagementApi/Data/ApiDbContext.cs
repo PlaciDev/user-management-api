d6f25d57b029a662a222aa814b80956736b8e116
PlaciDev <brunoplacidev@gmail.com>
2025-07-02 22:24:18 -0300

    recria e reconfigura do ApiDbcontext

---
+++ UseManagementApi/Data/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UseManagementApi.Models;
+
+namespace UseManagementApi.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) {}
+    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    
+    
+}
