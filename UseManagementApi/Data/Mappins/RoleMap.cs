97130a5a40f290c4ec054bbb020d391704e6a306
PlaciDev <brunoplacidev@gmail.com>
2025-07-02 22:37:40 -0300

    configura o mapeamento de Role

---
+++ UseManagementApi/Data/Mappins/RoleMap.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UseManagementApi.Models;
+
+namespace UseManagementApi.Data.Mappins;
+
+public class RoleMap : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("Role");
+
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .UseIdentityColumn();
+        
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnName("Name")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(15);
+        
+        
+    }
+}
